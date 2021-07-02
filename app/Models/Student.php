af49e46915fe8f5a88f175f53de69fa84072aeeb
Malak <malak.abuhammad@gmail.com>
2021-07-02 13:10:06 +0300

    main entities

---
+++ app/Models/Student.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $table = 'students';
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+    ];
+}
